38df2e33289469fc21b0f6ab9543a0d7c34158ef
guipalm4 <gomes.lmc@gmail.com>
2022-09-18 14:15:39 -0300

    Add Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,34 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+})
 
 app.get('/ads', (request, response) => {
 
   return response.json([
     {id:1, name: 'Anuncio 1'},
     {id:2, name: 'Anuncio 2'},
-    {id:3, name: 'Anuncio 3'}
+    {id:3, name: 'Anuncio 3'},
   ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+
+  return response.status(200).json([])
+
+})
+
+
+app.get('/game/:id/ads',(request, response) => {
+  const id = request.params.id
+  return response.status(200).json(id)
 })
 
 app.listen(3333);
